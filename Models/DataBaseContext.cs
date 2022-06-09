78fd59ae4e7aaa8fd4060fa81810bd4b1108ed1b
asifr <asifr@DESKTOP-IHD8HLU>
2022-06-09 07:36:22 +0530

    Add project files.

---
+++ Models/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentAttendanceProject.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<AttendanceModel> Attendances { get; set; }
+    }
+}
